11eb87dd765cbf53267e89f7b6f254e7ae11a2ca
Sara Smarandoiu <smarandoiusara@gmail.com>
2024-11-22 03:17:31 +0200

    NotAllowedFood

--- controllers/products/getNotAllowedProducts.js
+++ controllers/products/getNotAllowedProducts.js
@@ -1,12 +1,17 @@
-
 const { Product } = require("../../models");
 
 const getNotAllowedProducts = async bloodType => {
+    if (bloodType < 1 || bloodType > 4) {
+        throw new Error("Invalid blood type");
+    }
+
     const blood = [null, false, false, false, false];
     blood[bloodType] = true;
-    const products = Product.find({
+
+    const products = await Product.find({
         groupBloodNotAllowed: { $all: [blood] },
     });
+
     return products;
 };
 
